8f05e8ee390a92e21498fd34890a79315fc81f03
tqjav <tqjav@Khadija_A>
2023-04-11 17:48:28 +0530

    pushed

---
+++ project2/src/project2/Addition.java
@@ -0,0 +1,16 @@
+package project2;
+
+public class Addition {
+	
+	void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+
+	public static void main(String[] args) {
+		Addition a=new Addition();
+		a.add(23,5);
+
+	}
+
+}
---
+++ project2/src/project2/Subtraction.java
@@ -0,0 +1,15 @@
+package project2;
+
+public class Subtraction {
+	void add(int a,int b)
+	{
+		System.out.println(a-b);
+	}
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
